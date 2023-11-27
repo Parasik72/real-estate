456b8947072b861190d56352d8b232b35b0e9af9
Parasik72 <d.vorobec72@gmail.com>
2023-11-27 16:13:28 +0200

    feat: Implemented about us block UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ModernLivingImg from '../common/images/header picture.jpg';
 import CardImg1 from '../common/images/card-img-1.png';
+import AboutUsImg from '../common/images/about-us.png';
 import { FormSearch } from "@/common/components/home/form-search.component";
 import { ArrowIcon } from "@/common/icons/arrow.icon";
 
@@ -9,17 +10,23 @@ export default function Home() {
     <div className="antialiased py-8 w-full overflow-x-hidden">
       <div className="relative px-4 py-12 md:max-w-3xl md:mx-auto xl:px-0 lg:max-w-full xl:max-w-6xl">
         <div className="lg:max-w-lg">
-          <h1 className="text-dark-blue font-bold text-4xl lg:text-6.5xl lg:mt-4">Modern living for everyone</h1>
+          <h1 className="text-dark-blue font-bold text-4xl lg:text-6.5xl lg:mt-4">
+            Modern living for everyone
+          </h1>
           <div className="flex justify-center lg:block lg:absolute lg:inset-y-0 lg:right-0 xl:-right-14">
             <Image src={ModernLivingImg} width={580} height={557} alt="modern house" />
           </div>
-          <p className="mt-4 text-dark-blue text-xl leading-8 font-light z-10 relative">We provide a complete service for the sale, purchase or rental of real estate. We have been operating in Madrid and Barcelona more than 15 years.</p>
+          <p className="mt-4 text-dark-blue text-xl leading-8 font-light z-10 relative">
+            We provide a complete service for the sale or purchase of real estate. We have been operating in Madrid and Barcelona more than 15 years.
+          </p>
         </div>
         <FormSearch />
       </div>
-      <div className="bg-indigo-50 mt-36 w-full py-28">
+      <div className="bg-indigo-50 w-full py-14 lg:mt-36 lg:py-28">
         <div className="relative px-4 md:max-w-3xl md:mx-auto xl:px-0 lg:max-w-full xl:max-w-6xl">
-          <h2 className="text-dark-blue text-4xl lg:text-5xl font-bold">Last offers</h2>
+          <h2 className="text-dark-blue text-4xl lg:text-6xl font-bold">
+            Last offers
+          </h2>
           <div className="flex flex-col gap-5 md:gap-0 md:flex-row justify-between">
             <p className="mt-4 text-dark-blue text-xl leading-8 font-light max-w-xl">
               Fulfill your career dreams, enjoy all the achievements of the city center and luxury housing to the fullest.
@@ -58,6 +65,28 @@ export default function Home() {
                 </div>
               </div>
             ))}
+          </div>
+        </div>
+      </div>
+      <div className="py-14 lg:py-28">
+        <div className="relative px-4 md:max-w-3xl md:mx-auto xl:px-0 lg:max-w-full xl:max-w-6xl">
+          <div className="flex flex-col-reverse gap-10 md:flex-row md:gap-20 justify-between">
+              <div>
+                <Image src={AboutUsImg} width={688} height={464} alt="aboutUs" />
+              </div>
+              <div>
+                <h2 className="text-dark-blue text-4xl lg:text-6xl font-bold">
+                  About us
+                </h2>
+                <div className="flex flex-col gap-4">
+                  <p className="mt-4 text-dark-blue text-xl leading-8 font-light md:max-w-xl">
+                    We are a company that connects the world of real estate and finance. We provide a complete service for the sale or purchase of real estate. Our advantage is more than 15 years of experience and soil in attractive locations in Slovakia with branches in Bratislava and Košice.
+                  </p>
+                  <p className="mt-4 text-dark-blue text-xl leading-8 font-light md:max-w-xl">
+                    We have a connection to all banks on the Slovak market, so we can solve everything under one roof. By constantly innovating our business activities, we move forward and we are able to offer truly above-standard services that set us apart from the competition.
+                  </p>
+                </div>
+              </div>
           </div>
         </div>
       </div>
