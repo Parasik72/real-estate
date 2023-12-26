b7eec5fa6e0b9750f47d35c83256ba8af5d2100a
Parasik72 <d.vorobec72@gmail.com>
2023-12-26 14:53:10 +0200

    feat: Add scroll up to the top while changing route

--- common/components/layout.component.tsx
+++ common/components/layout.component.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import Navbar from "./navbar.component";
 import { Footer } from "./footer.component";
 import { AuthUserEffectActions } from "../services/auth-user/auth-user.service";
 import { Action, Dispatch } from "redux";
 import { connect } from "react-redux";
 import ToastifyContainer from "./toastify/toastify-container.component";
+import { useRouter } from "next/router";
 
 interface IProps {
   children?: React.ReactNode;
@@ -23,13 +24,20 @@ const mapDispatchToProps = (dispatch: Dispatch<Action<AuthUserEffectActions>>): 
 }
 
 function Layout({ children, getAuthUser }: IProps & IDispatch) {
+  const router = useRouter();
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    scrollRef.current?.scroll({ top: 0 });
+  }, [router.pathname]);
+
   useEffect(() => {
     getAuthUser();
   }, []);
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <div className="flex flex-col flex-1 overflow-y-auto">
+      <div ref={scrollRef} className="flex flex-col flex-1 overflow-y-auto">
         <main className="flex-1">
           {children}
         </main>
